b3881cf5b4898300877bba8b74299e7695f550c8
Irina <prilepovaid@mail.ru>
2021-07-27 15:45:52 +0700

    copy schemas-files from main-backend

---
+++ lib/schemas/point.d.ts
@@ -0,0 +1,6 @@
+import Joi = require("joi");
+export declare const pointGeoSchema: Joi.StringSchema;
+export declare const pointTextSchema: Joi.StringSchema;
+export declare const pointIdSchema: Joi.StringSchema;
+export declare const pointObjectSchema: Joi.ObjectSchema<any>;
+export declare const pointsArraySchema: Joi.ArraySchema;
---
+++ lib/schemas/user.d.ts
@@ -0,0 +1,9 @@
+import * as Joi from "joi";
+export declare const emailSchema: Joi.StringSchema;
+export declare const passwordSchema: Joi.StringSchema;
+export declare const firstNameSchema: Joi.StringSchema;
+export declare const lastNameSchema: Joi.StringSchema;
+export declare const avatarSchema: Joi.StringSchema;
+export declare const userIdSchema: Joi.StringSchema;
+export declare const userRoleSchema: Joi.StringSchema;
+export declare const userSchema: Joi.ObjectSchema<any>;
