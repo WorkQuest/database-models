5f94745c1435847898c31fb649f71f72c698d7bb
Irina Prilepova <prilepovaid@mail.ru>
2021-08-04 17:58:33 +0700

    Improve

--- lib/models/Session.d.ts
+++ lib/models/Session.d.ts
@@ -1,10 +1,7 @@
 import { Model } from "sequelize-typescript";
 import { User } from "./User";
-import { Admin } from "./Admin";
 export declare class Session extends Model {
     id: string;
     userId: string;
-    adminId: string;
     user: User;
-    admin: Admin;
 }
--- src/models/Session.ts
+++ src/models/Session.ts
@@ -1,15 +1,12 @@
 import { BelongsTo, Column, DataType, ForeignKey, Model, Table } from "sequelize-typescript";
 import { getUUID } from "../utils";
 import { User } from "./User";
-import { Admin } from "./Admin";
 
 
 @Table
 export class Session extends Model {
   @Column({ primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID() }) id: string;
   @ForeignKey(() => User) @Column(DataType.STRING) userId: string;
-  @ForeignKey(() => Admin) @Column(DataType.STRING) adminId: string;
 
   @BelongsTo(() => User) user: User;
-  @BelongsTo(() => Admin) admin: Admin;
 }
