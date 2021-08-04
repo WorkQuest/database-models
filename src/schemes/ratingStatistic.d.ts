80c7be5c2a4064750939fb682eb7fc22de0491ad
Microsxema <robertgtr@protonmail.com>
2021-08-04 17:01:26 +0700

    [project]: fix

--- src/schemes/ratingStatistic.d.ts
+++
@@ -1,9 +0,0 @@
-import * as Joi from 'joi';
-export declare const ratingStatisticReviewCountSchema: Joi.NumberSchema;
-export declare const ratingStatisticAverageMarkSchema: Joi.NumberSchema;
-export declare const ratingStatisticSchema: {
-    id: Joi.StringSchema;
-    userId: Joi.StringSchema;
-    reviewCount: Joi.NumberSchema;
-    averageMark: Joi.NumberSchema;
-};
