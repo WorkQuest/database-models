1b99b2574ec3c3bfe167861111732d5facbf2bb8
Irina Prilepova <prilepovaid@mail.ru>
2021-08-04 18:11:50 +0700

    Remove field

--- lib/models/AdminSession.d.ts
+++ lib/models/AdminSession.d.ts
@@ -2,7 +2,6 @@ import { Model } from "sequelize-typescript";
 import { Admin } from "./Admin";
 export declare class AdminSession extends Model {
     id: string;
-    userId: string;
     adminId: string;
     admin: Admin;
 }
--- src/models/AdminSession.ts
+++ src/models/AdminSession.ts
@@ -7,7 +7,6 @@ import { Admin } from "./Admin";
 @Table
 export class AdminSession extends Model {
     @Column({ primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID() }) id: string;
-    @ForeignKey(() => User) @Column(DataType.STRING) userId: string;
     @ForeignKey(() => Admin) @Column(DataType.STRING) adminId: string;
 
     @BelongsTo(() => Admin) admin: Admin;
