79a76dc457fbbe515ea8bb6f9ee31d48817dd3d0
Irina Prilepova <prilepovaid@mail.ru>
2021-08-05 17:17:38 +0700

    save changes

--- src/models/AdminSession.ts
+++ src/models/AdminSession.ts
@@ -1,6 +1,5 @@
 import { BelongsTo, Column, DataType, ForeignKey, Model, Table } from "sequelize-typescript";
 import { getUUID } from "../utils";
-import { User } from "./User";
 import { Admin } from "./Admin";
 
 
