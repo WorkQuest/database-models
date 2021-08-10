5d6ede7b4518b77517b0b9a9e97536281b08d809
Irina Prilepova <prilepovaid@mail.ru>
2021-08-10 13:39:03 +0700

    Delete paranoid

--- src/models/Disputes.ts
+++ src/models/Disputes.ts
@@ -22,7 +22,7 @@ export const DisputeStatuses = Object.values(DisputeStatus)
     },
   },
 }))
-@Table({paranoid: true})
+@Table
 export class Disputes extends Model {
   @Column({ type: DataType.STRING, defaultValue: getUUID, primaryKey: true }) id: string;
 
