9d4af2dfbbfaa56dd975a7e1e554dcaf35e02d27
Irina Prilepova <prilepovaid@mail.ru>
2021-08-13 10:59:43 +0700

    Improve

--- lib/models/UserBlockReason.d.ts
+++ lib/models/UserBlockReason.d.ts
@@ -2,7 +2,7 @@ import { Model } from "sequelize-typescript";
 import { User, UserStatus } from "./User";
 export declare class UserBlockReason extends Model {
     id: string;
-    adminId: string;
+    userId: string;
     blockReason: string;
     previousStatus: UserStatus;
     user: User;
--- src/models/UserBlockReason.ts
+++ src/models/UserBlockReason.ts
@@ -5,7 +5,7 @@ import {User, UserStatus} from "./User";
 @Table
 export class UserBlockReason extends Model {
   @Column({ primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID() }) id: string;
-  @ForeignKey(() => User) @Column(DataType.STRING) adminId: string;
+  @ForeignKey(() => User) @Column(DataType.STRING) userId: string;
 
   @Column({type: DataType.TEXT, allowNull: false}) blockReason: string;
   @Column({type: DataType.INTEGER, allowNull: false}) previousStatus: UserStatus;
