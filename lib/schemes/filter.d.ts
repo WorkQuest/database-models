90272aed9b0661eab66a2b996ebeb1715662910a
Alexander Bokovoy <robzombieg@gmail.com>
2021-08-25 15:37:06 +0700

    update models

--- lib/schemes/filter.d.ts
+++ lib/schemes/filter.d.ts
@@ -1,4 +1,6 @@
 import * as Joi from "joi";
+export declare const filterIdSchema: Joi.AlternativesSchema;
+export declare const questFilterId: Joi.AlternativesSchema;
 export declare const questFilterSchema: Joi.ObjectSchema<any>;
 export declare const filterSchema: Joi.ArraySchema;
 export declare const questFilterSchemaResponse: Joi.ObjectSchema<any>;
--- src/schemes/filter.ts
+++ src/schemes/filter.ts
@@ -4,8 +4,9 @@ import {idSchema} from "./common";
 
 const skillsSchema = Joi.string().label('SkillsSchema')
 const nameCategory = Joi.string().label('NameCategory');
-const filterIdSchema = idSchema.label('FilterId');
-const questFilterId = idSchema.label('QuestIdFilter')
+
+export const filterIdSchema = Joi.alternatives().try(Joi.string().uuid(), Joi.string().default(null)).label("FilterId");
+export const questFilterId = Joi.alternatives().try(Joi.string().uuid(), Joi.string().default(null)).label("QuestIdFilter");
 const filterSkillsSchema = Joi.array().items(skillsSchema).label('SkillsSchema')
 
 export const questFilterSchema = Joi.object({
