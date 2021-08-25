d4a4f5e170989f987ab63ee79fd27df4047a8c23
Alexander Bokovoy <robzombieg@gmail.com>
2021-08-25 16:48:49 +0700

    update models

--- src/models/Filter.ts
+++ src/models/Filter.ts
@@ -16,8 +16,8 @@ import {User} from "./User";
 export class Filter extends Model {
     @Column({primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID()}) id: string;
 
-    @ForeignKey(() => User) @Column({type: DataType.STRING, defaultValue:null}) userId: string;
-    @ForeignKey(() => Quest) @Column({type: DataType.STRING, defaultValue:null}) questId: string;
+    @ForeignKey(() => User) @Column({type: DataType.STRING, allowNull: true, defaultValue:null}) userId: string;
+    @ForeignKey(() => Quest) @Column({type: DataType.STRING, allowNull: true, defaultValue:null}) questId: string;
     @Column({type: DataType.STRING, allowNull: false}) category: string;
     @Column({type: DataType.STRING, allowNull: false}) skills: string;
 
