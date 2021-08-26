c51b46db75b164c5041c939f67ffb08ffbd1934a
Alexander Bokovoy <robzombieg@gmail.com>
2021-08-26 12:29:54 +0700

    (Quest & User)

--- src/models/Quest.ts
+++ src/models/Quest.ts
@@ -68,7 +68,7 @@ export interface Location {
     }, {
       model: Filter,
       as: 'filter',
-      attributes: ["category", "skills"]
+      attributes: ["questId","category", "skills"]
     }]
   }
 }))
