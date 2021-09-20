6190746529393b6caf101b58c78f0bd923fd2d29
Microsxema <robertgtr@protonmail.com>
2021-09-20 11:16:25 +0700

    rebuild

--- lib/schemes/user.d.ts
+++ lib/schemes/user.d.ts
@@ -5,8 +5,6 @@ export declare const userFirstNameSchema: Joi.StringSchema;
 export declare const userLastNameSchema: Joi.StringSchema;
 export declare const userStatusSchema: Joi.NumberSchema;
 export declare const userRoleSchema: Joi.StringSchema;
-export declare const userPhoneSchema: Joi.StringSchema;
-export declare const userTempPhoneSchema: Joi.StringSchema;
 export declare const userSocialMediaNicknamesSchema: Joi.ObjectSchema<any>;
 export declare const userKnowledgeSchema: Joi.ObjectSchema<any>;
 export declare const userWorkExperienceSchema: Joi.ObjectSchema<any>;
