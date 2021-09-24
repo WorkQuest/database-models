e8304b4b939caa94b9291c1242257351de1e5bb2
Irina Prilepova <prilepovaid@mail.ru>
2021-09-24 18:34:47 +0700

    [RatingStatistic]: add status for RatingStatistic

--- lib/models/user/RatingStatistic.d.ts
+++ lib/models/user/RatingStatistic.d.ts
@@ -1,9 +1,16 @@
 import { Model } from 'sequelize-typescript';
 import { User } from './User';
+declare enum RatingStatus {
+    verify = "verified",
+    reliable = "reliable",
+    topRanked = "topRanked"
+}
 export declare class RatingStatistic extends Model {
     id: string;
     userId: string;
     reviewCount: number;
     averageMark: number;
+    status: RatingStatus;
     user: User;
 }
+export {};
