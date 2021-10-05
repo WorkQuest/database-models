e671d8b382b3821ef18bf12402cac413f49bea50
Microsxema <robertgtr@protonmail.com>
2021-10-05 11:08:02 +0700

    rebuild

--- lib/models/ExportModels.d.ts
+++
@@ -1,5 +0,0 @@
-import { Model } from 'sequelize-typescript';
-export declare class BlockTransaction extends Model {
-    blockNumber: number;
-    transactionHash: string;
-}
--- lib/models/swapListener/ParserBlockInfo.d.ts
+++
@@ -1,6 +0,0 @@
-import { Model } from 'sequelize-typescript';
-export declare class ParserBlockInfo extends Model {
-    lastParsedBlock: number;
-    network: string;
-    contract: string;
-}
