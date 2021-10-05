9713056c88b7289df500d5477f2936272a225cd8
Microsxema <robertgtr@protonmail.com>
2021-10-05 15:22:43 +0700

    rebuild

--- lib/schemes/map.d.ts
+++
@@ -1,7 +0,0 @@
-import * as Joi from "joi";
-export declare const mapPointsCountSchema: Joi.NumberSchema;
-export declare const mapTypeSchema: Joi.StringSchema;
-export declare const mapCoordinatesSchema: Joi.ArraySchema;
-export declare const mapClusterRadiusSchema: Joi.NumberSchema;
-export declare const mapPointSchema: Joi.ObjectSchema<any>;
-export declare const mapPointsSchema: Joi.ArraySchema;
--- lib/schemes/ratingStatistic.d.ts
+++
@@ -1,4 +0,0 @@
-import * as Joi from 'joi';
-export declare const ratingStatisticReviewCountSchema: Joi.NumberSchema;
-export declare const ratingStatisticAverageMarkSchema: Joi.NumberSchema;
-export declare const ratingStatisticSchema: Joi.ObjectSchema<any>;
