74cce33f56149b50bcfd891733b50688e5e00ba1
Microsxema <robertgtr@protonmail.com>
2021-10-05 15:23:04 +0700

    rebuild

---
+++ lib/models/SkillFilter.d.ts
@@ -0,0 +1,23 @@
+import { Model } from 'sequelize-typescript';
+import { Quest } from './quest/Quest';
+import { User } from "./user/User";
+export declare type SkillsMap = {
+    [category: string]: string[];
+};
+export declare type SkillsRaw = {
+    category: string;
+    skill: string;
+};
+export declare class SkillFilter extends Model {
+    id: string;
+    userId: string;
+    questId: string;
+    category: string;
+    skill: string;
+    user: User;
+    quest: Quest;
+    static toMapSkills(skillsRaw: SkillsRaw[]): SkillsMap;
+    static toRawSkills(skillsMap: SkillsMap, alias: string, id: string): SkillsRaw[];
+    static toRawUserSkills(skillsMap: SkillsMap, userId: string): SkillsRaw[];
+    static toRawQuestSkills(skillsMap: SkillsMap, questId: string): SkillsRaw[];
+}
---
+++ lib/utils/quest.d.ts
@@ -0,0 +1,2 @@
+import { LocationType, LocationPostGISType } from "../models/types";
+export declare function transformToGeoPostGIS(location: LocationType): LocationPostGISType;
