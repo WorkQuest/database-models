20502b06f52a356c03c8a39b34c7416167c265de
Microsxema <robertgtr@protonmail.com>
2021-10-12 17:04:43 +0700

    [Model, Schema]: update quest, user alias

---
+++ lib/models/chats/Chat.d.ts
@@ -0,0 +1,26 @@
+import { Model } from "sequelize-typescript";
+import { Message } from "./Message";
+import { ChatMember } from "./ChatMember";
+import { User } from "../user/User";
+export declare enum ChatType {
+    private = "private",
+    group = "group"
+}
+export declare class Chat extends Model {
+    id: string;
+    ownerUserId: string;
+    lastMessageId: string;
+    name: string;
+    type: ChatType;
+    lastMessageDate: Date;
+    members: User[];
+    owner: User;
+    lastMessage: Message;
+    messages: Message[];
+    chatMembers: ChatMember[];
+    firstMemberInPrivateChat: ChatMember;
+    secondMemberInPrivateChat: ChatMember;
+    mustHaveMember(userId: string): Promise<void>;
+    mustHaveType(type: ChatType): void;
+    mustHaveOwner(userId: String): void;
+}
---
+++ lib/schemes/ratingStatistic.d.ts
@@ -0,0 +1,4 @@
+import * as Joi from 'joi';
+export declare const ratingStatisticReviewCountSchema: Joi.NumberSchema;
+export declare const ratingStatisticAverageMarkSchema: Joi.NumberSchema;
+export declare const ratingStatisticSchema: Joi.ObjectSchema<any>;
