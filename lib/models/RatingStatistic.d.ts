1591c6da769b3140140b4180df090563eb7fb7ab
Irina Prilepova <prilepovaid@mail.ru>
2021-11-10 17:13:26 +0700

    Rebuild

--- lib/models/RatingStatistic.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from 'sequelize-typescript';
-import { User } from './user/User';
-export declare class RatingStatistic extends Model {
-    id: string;
-    userId: string;
-    reviewCount: number;
-    averageMark: number;
-    user: User;
-}
