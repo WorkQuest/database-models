754d84287081595db87b67fc77f7e7b5e5c24260
Irina Prilepova <prilepovaid@mail.ru>
2021-11-12 13:12:36 +0700

    [QuestBlockReason]: add allowNull: false to questIt column

--- src/models/quest/QuestBlockReason.ts
+++ src/models/quest/QuestBlockReason.ts
@@ -5,13 +5,12 @@ import {Quest, QuestStatus} from "./Quest";
 @Table
 export class QuestBlockReason extends Model {
   @Column({ primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID() }) id: string;
-  @ForeignKey(() => Quest) @Column(DataType.STRING) questId: string;
+  @ForeignKey(() => Quest) @Column({type: DataType.STRING, allowNull: false}) questId: string;
 
   @Column({type: DataType.TEXT, allowNull: false}) blockReason: string;
   @Column({type: DataType.INTEGER, allowNull: false}) previousStatus: QuestStatus;
 
   @Column({type: DataType.BOOLEAN}) isLast: boolean;
-
 
   @BelongsTo(() => Quest) quest: Quest;
 }
