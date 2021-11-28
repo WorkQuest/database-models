4164780720b55a13f673d1018e2dfab1eba1bce6
Microsxema <robertgtr@protonmail.com>
2021-11-28 18:05:03 +0700

    rebuild

--- lib/schemes/discussion.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Joi from "joi";
-export declare const discussionCommentTextSchema: Joi.StringSchema;
-export declare const discussionCommentSchema: Joi.ObjectSchema<any>;
-export declare const discussionTitleSchema: Joi.StringSchema;
-export declare const discussionDescriptionSchema: Joi.StringSchema;
-export declare const discussionSchema: Joi.ObjectSchema<any>;
-export declare const discussionsSchema: Joi.ArraySchema;
-export declare const discussionCommentsSchema: Joi.ArraySchema;
