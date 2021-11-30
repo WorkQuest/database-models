44a207b2e9d8e77e17b64daae5bf1824772c7bf2
Irina Prilepova <prilepovaid@mail.ru>
2021-11-30 17:05:31 +0700

    [StarredDiscussion]: add new table

---
+++ src/models/discussion/StarredDiscussion.ts
@@ -0,0 +1,19 @@
+import { BelongsTo, Column, DataType, ForeignKey, Model, Table } from 'sequelize-typescript';
+import { getUUID } from '../../utils';
+import { User } from '../user/User';
+import {Discussion} from "./Discussion";
+
+@Table
+export class StarredMessage extends Model {
+  @Column({ primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID() }) id: string;
+
+  @ForeignKey(() => User)
+  @Column({type: DataType.STRING, allowNull: false}) userId: string;
+
+  @ForeignKey(() => Discussion)
+  @Column({type: DataType.STRING, allowNull: false}) discussionId: string;
+
+  @BelongsTo(() => User, { constraints: false }) user: User;
+  @BelongsTo(() => Discussion, { constraints: false }) discussion: Discussion;
+}
+
