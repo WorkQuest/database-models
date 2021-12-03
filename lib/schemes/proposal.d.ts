d8516d53d7e88edb9b6c59460d0be8ad6c7d522d
Irina Prilepova <prilepovaid@mail.ru>
2021-12-03 12:32:47 +0700

    [proposal]: improve schemas

--- lib/schemes/proposal.d.ts
+++ lib/schemes/proposal.d.ts
@@ -2,6 +2,6 @@ import * as Joi from "joi";
 export declare const proposalNumberSchema: Joi.NumberSchema;
 export declare const proposalTitleSchema: Joi.StringSchema;
 export declare const proposalDescriptionSchema: Joi.StringSchema;
-export declare const proposalStatus: Joi.StringSchema;
-export declare const proposalTxHashSchema: Joi.NumberSchema;
+export declare const proposalStatus: Joi.NumberSchema;
+export declare const proposalTxHashSchema: Joi.StringSchema;
 export declare const proposalSchema: Joi.ObjectSchema<any>;
--- src/schemes/proposal.ts
+++ src/schemes/proposal.ts
@@ -1,12 +1,12 @@
 import * as Joi from "joi";
 import {idSchema} from "./common";
-import {ProposalStatus} from "../models/Proposal/Proposal";
+import {ProposalStatus} from "../models";
 
 export const proposalNumberSchema = Joi.number().example(1).label('ProposalNumber');
 export const proposalTitleSchema = Joi.string().example('New post').label('ProposalTitle');
 export const proposalDescriptionSchema = Joi.string().example('Hello world').label('ProposalDescription');
-export const proposalStatus = Joi.string().valid(...Object.values(ProposalStatus)).example(ProposalStatus.Pending).label("ProposalStatus");
-export const proposalTxHashSchema = Joi.number().label("ProposalCreatorHash");
+export const proposalStatus = Joi.number().valid(...Object.values(ProposalStatus)).example(ProposalStatus.Pending).label("ProposalStatus");
+export const proposalTxHashSchema = Joi.string().example('18vk40cc3er48fzs5ghqzxy88uq').label("ProposalCreatorHash");
 
 export const proposalSchema = Joi.object({
   id: idSchema,
