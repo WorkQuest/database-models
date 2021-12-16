20cfb27b82c84b7762e68635f306f1a3f5dabfa5
Alexander Bokovoy <robzombieg@gmail.com>
2021-12-16 14:25:13 +0700

    [feature] last update DB Proposal

--- src/models/proposal/Proposal.ts
+++ src/models/proposal/Proposal.ts
@@ -3,61 +3,48 @@ import {Media} from "../Media";
 import {User} from "../user/User";
 import {ProposalMedia} from "./ProposalMedia";
 import {
-    Model,
-    Table,
-    Column,
-    Scopes,
-    HasMany,
-    DataType,
-    BelongsTo,
-    ForeignKey,
-    BelongsToMany,
+  Model,
+  Table,
+  Column,
+  Scopes,
+  DataType,
+  BelongsTo,
+  ForeignKey,
+  BelongsToMany,
 } from "sequelize-typescript";
 
 export enum ProposalStatus {
-    Pending,
-    Active,
-    Rejected,
-    Accepted,
+  Pending,
+  Active,
+  Rejected,
+  Accepted,
 }
 
 @Scopes(() => ({
-    defaultScope: {
-        attributes: {exclude: ["proposalId", "votingPeriod", "minimumQuorum", "timestamp", "txHash", "updatedAt", "createdAt", "deletedAt"]},
-        include: [{
-            model: Media.scope("urlOnly"),
-            as: "medias",
-            through: {attributes: []}
-        }]
-    },
+  defaultScope: {
+    attributes: {exclude: ["proposalId", "votingPeriod", "minimumQuorum", "timestamp", "txHash", "updatedAt", "createdAt", "deletedAt"]},
+      include: [{
+        model: Media.scope("urlOnly"),
+        as: "medias",
+        through: {attributes: []}
+    }]
+  },
 }))
 @Table({paranoid: true})
 export class Proposal extends Model {
-    @Column({primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID()}) id: string;
-
-    @ForeignKey(() => User)
-    @Column({type: DataType.STRING, allowNull: false}) userId: string;
-
-    @Column({type: DataType.STRING, allowNull: false}) proposer: string;
-
-    @Column({type: DataType.DECIMAL, defaultValue: () => getUUIDInt(), unique: true}) nonce: string;
-
-    @Column({type: DataType.INTEGER, defaultValue: null}) proposalId: number;
-
-    @Column({type: DataType.STRING, allowNull: false}) title: string;
-
-    @Column({type: DataType.TEXT, allowNull: false}) description: string;
-
-    @Column({type: DataType.INTEGER, defaultValue: null}) votingPeriod: number;
-
-    @Column({type: DataType.INTEGER, defaultValue: null}) minimumQuorum: number;
-
-    @Column({type: DataType.INTEGER, defaultValue: ProposalStatus.Pending}) status: ProposalStatus;
-
-    @Column({type: DataType.INTEGER, defaultValue: null}) timestamp: number;
-
-    @Column({type: DataType.STRING, defaultValue: null}) txHash: string;
-
-    @BelongsTo(() => User) author: User;
-    @BelongsToMany(() => Media, () => ProposalMedia) medias: Media[];
+  @Column({primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID()}) id: string;
+  @ForeignKey(() => User)
+  @Column({type: DataType.STRING, allowNull: false}) userId: string;
+  @Column({type: DataType.STRING, allowNull: false}) proposer: string;
+  @Column({type: DataType.DECIMAL, defaultValue: () => getUUIDInt(), unique: true}) nonce: string;
+  @Column({type: DataType.INTEGER, defaultValue: null}) proposalId: number;
+  @Column({type: DataType.STRING, allowNull: false}) title: string;
+  @Column({type: DataType.TEXT, allowNull: false}) description: string;
+  @Column({type: DataType.INTEGER, defaultValue: null}) votingPeriod: number;
+  @Column({type: DataType.INTEGER, defaultValue: null}) minimumQuorum: number;
+  @Column({type: DataType.INTEGER, defaultValue: ProposalStatus.Pending}) status: ProposalStatus;
+  @Column({type: DataType.INTEGER, defaultValue: null}) timestamp: number;
+  @Column({type: DataType.STRING, defaultValue: null}) txHash: string;
+  @BelongsTo(() => User) author: User;
+  @BelongsToMany(() => Media, () => ProposalMedia) medias: Media[];
 }
--- src/models/proposal/ProposalExecuted.ts
+++ src/models/proposal/ProposalExecuted.ts
@@ -2,22 +2,22 @@ import {Model, Column, DataType, Table, Scopes} from 'sequelize-typescript';
 import {BlockchainNetworks} from "../types";
 
 export enum ProposalExecutedEventType {
-    ProposalExecuted = 'ProposalExecuted'
+  ProposalExecuted = 'ProposalExecuted'
 }
 
 @Scopes(() => ({
-    defaultScope: {
-        attributes: {
-            exclude: ["id", "network", "event"]
-        }
+  defaultScope: {
+    attributes: {
+      exclude: ["id", "network", "event"]
     }
+  }
 }))
 @Table
 export class ProposalExecuted extends Model {
-    @Column(DataType.STRING) transactionHash: string;
-    @Column(DataType.INTEGER) proposalId: number;
-    @Column(DataType.BOOLEAN) succeeded: boolean;
-    @Column(DataType.BOOLEAN) defeated: boolean;
-    @Column(DataType.STRING) network: BlockchainNetworks;
-    @Column(DataType.STRING) event: ProposalExecutedEventType;
+  @Column(DataType.STRING) transactionHash: string;
+  @Column(DataType.INTEGER) proposalId: number;
+  @Column(DataType.BOOLEAN) succeeded: boolean;
+  @Column(DataType.BOOLEAN) defeated: boolean;
+  @Column(DataType.STRING) network: BlockchainNetworks;
+  @Column(DataType.STRING) event: ProposalExecutedEventType;
 }
