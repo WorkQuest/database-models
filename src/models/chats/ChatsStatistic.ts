4745c2803d48012bf0b3971522cc4ea8f59e5d18
Irina Prilepova <prilepovaid@mail.ru>
2021-12-23 12:36:59 +0700

    build(ChatStatistic): create new table for chats unread messages statistic

---
+++ src/models/chats/ChatsStatistic.ts
@@ -0,0 +1,14 @@
+import {BelongsTo, Column, DataType, ForeignKey, Model, Table} from "sequelize-typescript";
+import { getUUID } from "../../utils";
+import {User} from "../user/User";
+
+@Table
+export class ChatsStatistic extends Model {
+  @Column({ primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID() }) id: string;
+
+  @ForeignKey(() => User) @Column(DataType.STRING) userId: string;
+
+  @Column({type: DataType.INTEGER, defaultValue: 0}) unreadMessages: number;
+
+  @BelongsTo(() => User) user: User;
+}
