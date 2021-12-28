219f71a0a47318eaaec86c78fc22cbb47f0d9520
Microsxema <robertgtr@protonmail.com>
2021-12-28 16:24:25 +0700

    rebuild

--- lib/models/proposal/ProposalExecutedEvent.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from 'sequelize-typescript';
-import { BlockchainNetworks } from "../types";
-export declare class ProposalExecutedEvent extends Model {
-    transactionHash: string;
-    proposalId: number;
-    succeeded: boolean;
-    defeated: boolean;
-    network: BlockchainNetworks;
-}
