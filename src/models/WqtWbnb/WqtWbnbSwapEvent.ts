a5120829048c788459167a2b245591cdf8b80d6a
Microsxema <robertgtr@protonmail.com>
2022-01-18 18:47:27 +0700

    rebuild

--- src/models/WqtWbnb/WqtWbnbSwapEvent.ts
+++ src/models/WqtWbnb/WqtWbnbSwapEvent.ts
@@ -3,7 +3,7 @@ import {Column, DataType, Model, Table} from "sequelize-typescript";
 @Table
 export class WqtWbnbSwapEvent extends Model {
   @Column(DataType.INTEGER) blockNumber: number;
-  @Column(DataType.STRING) totalUSD: string; /** In decimal */
+  @Column(DataType.STRING) totalUSD: string;
   @Column(DataType.STRING) bnbAmountIn: string; /** In decimal */
   @Column(DataType.STRING) bnbAmountOut: string; /** In decimal */
   @Column(DataType.STRING) wqtAmountIn: string; /** In decimal */
