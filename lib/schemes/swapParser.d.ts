6718638530349d5f28bbefa8fa2d50901d9d84d4
Irina Prilepova <prilepovaid@mail.ru>
2022-01-18 14:50:35 +0700

    Rebuild

--- lib/schemes/swapParser.d.ts
+++
@@ -1,4 +0,0 @@
-import Joi = require("joi");
-export declare const coinAmountSchema: Joi.StringSchema;
-export declare const accountAddressSchema: Joi.StringSchema;
-export declare const wqtWbnbSwapEvenSchema: Joi.ObjectSchema<any>;
