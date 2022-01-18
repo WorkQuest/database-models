8d7a91637df593b03a193d6483c46cd31d1b1078
Microsxema <robertgtr@protonmail.com>
2022-01-18 14:07:38 +0700

    add QuestCreatedEvent

---
+++ src/models/quest/QuestCreatedEvent.ts
@@ -0,0 +1,16 @@
+import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {getUUID} from "../../utils";
+
+@Table /** Contract event */
+export class QuestCreatedEvent extends Model {
+  // @Column({ primaryKey: true, type: DataType.STRING, allowNull: false }) address: string;
+  //
+  // employer
+  // nonce
+  //
+  // jobHash
+  // employer
+  // workquest
+  //
+  // nonce
+}
