819247ce04edf2f838541932ad194f7819c0eb17
Irina Prilepova <prilepovaid@mail.ru>
2022-01-19 18:48:17 +0700

    refactor(Schemes): add network field

--- lib/schemes/wqtWbnb.d.ts
+++ lib/schemes/wqtWbnb.d.ts
@@ -1,5 +1,6 @@
 import Joi = require("joi");
 export declare const coinAmountSchema: Joi.StringSchema;
 export declare const accountAddressSchema: Joi.StringSchema;
+export declare const networkSchema: Joi.StringSchema;
 export declare const wqtWbnbSwapEventSchema: Joi.ObjectSchema<any>;
 export declare const wqtWbnbSwapEventsSchema: Joi.ArraySchema;
--- src/schemes/wqtWbnb.ts
+++ src/schemes/wqtWbnb.ts
@@ -1,9 +1,11 @@
 import Joi = require("joi");
 import {blockNumberSchema, numericIdSchema, timestampSchema, transactionHashSchema} from "./common";
-import {discussionSchema} from "./discussion";
+import {BlockchainNetworks, WorkPlace} from "../models";
 
 export const coinAmountSchema = Joi.string().example("281231").label("CoinAmount");
 export const accountAddressSchema = Joi.string().example("0xke2083852Ccf274D48E149F99c80a5c742693418").label("AccountAddressSchema");
+export const networkSchema = Joi.string().valid(...Object.values(BlockchainNetworks)).example(BlockchainNetworks.bscMainNetwork).label('Network');
+
 
 export const wqtWbnbSwapEventSchema = Joi.object({
   id: numericIdSchema,
@@ -16,6 +18,7 @@ export const wqtWbnbSwapEventSchema = Joi.object({
   account: accountAddressSchema, // TODO in common
   timestamp: timestampSchema,
   transactionHash: transactionHashSchema,
+  network: networkSchema,
 }).label('WqtWbnbSwapEvent');
 
 export const wqtWbnbSwapEventsSchema = Joi.array().items(wqtWbnbSwapEventSchema).label('WqtWbnbSwapEvents');
