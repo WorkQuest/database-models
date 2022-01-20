3dbf2b14b95234af32149caa7f01ad15e83598bc
Microsxema <robertgtr@protonmail.com>
2022-01-20 15:11:33 +0700

    fif code style

--- src/schemes/wqtWbnb.ts
+++ src/schemes/wqtWbnb.ts
@@ -2,7 +2,7 @@ import Joi = require("joi");
 import {blockNumberSchema, timestampSchema, transactionHashSchema} from "./common";
 
 export const coinAmountSchema = Joi.string().example("281231").label("CoinAmount");
-export const accountAddressSchema = Joi.string().example("0xke2083852Ccf274D48E149F99c80a5c742693418").label("AccountAddressSchema");
+export const accountAddressSchema = Joi.string().example("0xke2083852Ccf274D48E149F99c80a5c742693418").label("AccountAddress");
 
 export const wqtWbnbSwapEventSchema = Joi.object({
   blockNumber: blockNumberSchema,
