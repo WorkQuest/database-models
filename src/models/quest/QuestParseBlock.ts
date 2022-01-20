aad7e8e5d1ddd52f0c3f2b16441ea99b3465fe01
Ilya Kasimov <adamoffilya@mail.ru>
2022-01-20 15:14:26 +0700

    Add QuestParseBlock

---
+++ src/models/quest/QuestParseBlock.ts
@@ -0,0 +1,8 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { BlockchainNetworks } from "../types";
+
+@Table
+export class QuestParseBlock extends Model {
+  @Column({type: DataType.INTEGER, defaultValue: 0}) lastParsedBlock: number;
+  @Column({type: DataType.STRING, allowNull: false, unique: true}) network: BlockchainNetworks;
+}
