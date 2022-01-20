fff4103c977fca66da2a287696e772e408419222
Microsxema <robertgtr@protonmail.com>
2022-01-20 13:43:13 +0700

    rebuild

--- src/schemes/wqtWbnb.ts
+++ src/schemes/wqtWbnb.ts
@@ -1,14 +1,10 @@
 import Joi = require("joi");
-import {blockNumberSchema, numericIdSchema, timestampSchema, transactionHashSchema} from "./common";
-import {BlockchainNetworks, WorkPlace} from "../models";
+import {blockNumberSchema, timestampSchema, transactionHashSchema} from "./common";
 
 export const coinAmountSchema = Joi.string().example("281231").label("CoinAmount");
 export const accountAddressSchema = Joi.string().example("0xke2083852Ccf274D48E149F99c80a5c742693418").label("AccountAddressSchema");
-export const networkSchema = Joi.string().valid(...Object.values(BlockchainNetworks)).example(BlockchainNetworks.bscMainNetwork).label('Network');
-
 
 export const wqtWbnbSwapEventSchema = Joi.object({
-  id: numericIdSchema,
   blockNumber: blockNumberSchema,
   totalUSD: coinAmountSchema,
   amount0In: coinAmountSchema,
@@ -18,7 +14,6 @@ export const wqtWbnbSwapEventSchema = Joi.object({
   to: accountAddressSchema, // TODO in common
   timestamp: timestampSchema,
   transactionHash: transactionHashSchema,
-  network: networkSchema,
 }).label('WqtWbnbSwapEvent');
 
 export const wqtWbnbSwapEventsSchema = Joi.array().items(wqtWbnbSwapEventSchema).label('WqtWbnbSwapEvents');
