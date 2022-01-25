20a030da520e4dcd6daa28b02ce20c51e920c205
Irina Prilepova <prilepovaid@mail.ru>
2022-01-25 12:01:40 +0700

    build(types): add BlackListStatus

--- lib/schemes/questBlackList.d.ts
+++ lib/schemes/questBlackList.d.ts
@@ -1,3 +1,4 @@
 import * as Joi from "joi";
 export declare const questBlockReasonSchema: Joi.StringSchema;
+export declare const questBlockStatusSchema: Joi.NumberSchema;
 export declare const questBlackListSchema: Joi.ObjectSchema<any>;
--- src/schemes/questBlackList.ts
+++ src/schemes/questBlackList.ts
@@ -1,11 +1,14 @@
 import * as Joi from "joi";
 import {idSchema} from "./common";
+import {BlackListStatus} from "../models";
 
 export const questBlockReasonSchema = Joi.string().example('Quest was blocked').label('QuestBlockReason');
+export const questBlockStatusSchema = Joi.number().valid(...Object.keys(BlackListStatus).map(key => parseInt(key)).filter(key => !isNaN(key))).example(BlackListStatus.Blocked).label('QuestBlockStatus');
 
 export const questBlackListSchema = Joi.object({
   id: idSchema,
   adminId: idSchema,
   questId: idSchema,
   reason: questBlockReasonSchema,
+  status: questBlockStatusSchema,
 }).label('QuestBlackList');
