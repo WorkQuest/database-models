059b501b401236294583ba30252a8368295e9a49
Irina Prilepova <prilepovaid@mail.ru>
2022-01-28 12:14:52 +0700

    refactor(questRaiseView): add payAmount scheme

--- lib/schemes/questRaiseView.d.ts
+++ lib/schemes/questRaiseView.d.ts
@@ -2,4 +2,5 @@ import * as Joi from "joi";
 export declare const questRaiseTypeScheme: Joi.NumberSchema;
 export declare const questRaiseStatusSchema: Joi.NumberSchema;
 export declare const questRaiseDurationSchema: Joi.NumberSchema;
+export declare const questRaisePayAmountSchema: Joi.NumberSchema;
 export declare const questRaiseViewSchema: Joi.ObjectSchema<any>;
