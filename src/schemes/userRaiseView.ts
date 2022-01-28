7aca38c2dc685f92f8d2aeb823fc919313cdfc5e
Irina Prilepova <prilepovaid@mail.ru>
2022-01-28 16:30:53 +0700

    refactor(userRaiseView): add userRaisePayAmountSchema

--- src/schemes/userRaiseView.ts
+++ src/schemes/userRaiseView.ts
@@ -5,6 +5,7 @@ import {userShortSchema} from "./user";
 
 export const userRaiseStatusSchema = Joi.number().valid(...Object.keys(UserRaiseStatus).map(key => parseInt(key)).filter(key => !isNaN(key))).example(UserRaiseStatus.Unpaid).label('UserRaiseStatus');
 export const userRaiseDurationSchema = Joi.number().valid(...Object.keys(UserRaiseDuration).map(key => parseInt(key)).filter(key => !isNaN(key))).example(UserRaiseDuration.OneDay).label('UserRaiseDuration');
+export const userRaisePayAmountSchema = Joi.number().example(10).label('QuestRaisePayAmount');
 
 export const userRaiseViewSchema = Joi.object({
   id: idSchema,
