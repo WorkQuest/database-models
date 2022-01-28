0353145e7ef2531a8b19cf603661f0d4cfeea728
Alexander Bokovoy <robzombieg@gmail.com>
2022-01-28 10:47:24 +0700

    [update] update build

--- lib/models/user/User.d.ts
+++ lib/models/user/User.d.ts
@@ -12,6 +12,7 @@ import { Chat } from "../chats/Chat";
 import { QuestsStatistic } from "../quest/QuestsStatistic";
 import { Wallet } from "../wallet/Wallet";
 import { ChatsStatistic } from "../chats/ChatsStatistic";
+import { AdminChangeRole } from "../admin/AdminChangeRole";
 export interface SocialInfo {
     id: string;
     email: string;
@@ -118,6 +119,7 @@ export declare class User extends Model {
     chatMember: ChatMember;
     userIndustryForFiltering: UserSpecializationFilter;
     userSpecializationForFiltering: UserSpecializationFilter;
+    userChangeRole: AdminChangeRole;
     chatsOfUser: Chat[];
     chatMembers: ChatMember[];
     discussionLikes: DiscussionLike[];
