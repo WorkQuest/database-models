5f5ce312f2935cee5bab6d456f05f7c323ed2fe4
Irina Prilepova <prilepovaid@mail.ru>
2022-02-14 11:13:52 +0700

    refactor(index): add model QuestDisputeReview into index

--- src/models/index.ts
+++ src/models/index.ts
@@ -56,6 +56,7 @@ import {QuestBlackList} from "./quest/QuestBlackList";
 import {WqtWbnbBurnEvent} from "./WqtWbnb/WqtWbnbBurnEvent";
 import {WqtWbnbMintEvent} from "./WqtWbnb/WqtWbnbMintEvent";
 import {UserChangeRoleData} from "./user/UserChangeRoleData";
+import {QuestDisputeReview} from "./quest/QuestDisputeReview";
 
 export async function initDatabase(dbLink: string, logging = false, sync = false) {
   const sequelize = new Sequelize(dbLink, {
@@ -82,6 +83,7 @@ export async function initDatabase(dbLink: string, logging = false, sync = false
       QuestDispute,
       StarredQuests,
       QuestsResponse,
+      QuestDisputeReview,
 
 
       /** Chat section */
@@ -226,3 +228,4 @@ export * from "./pensionFund/PensionFundWithdrewEvent";
 export * from "./user/UserBlackList";
 export * from "./quest/QuestBlackList";
 export * from "./user/UserChangeRoleData";
+export * from "./quest/QuestDisputeReview";
