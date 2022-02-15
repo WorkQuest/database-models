759a99f71de039ddc28f4dce9d2a2198e94f1eff
Irina Prilepova <prilepovaid@mail.ru>
2022-02-15 16:21:59 +0700

    Rebuild

--- src/models/chats/GroupChat.ts
+++ src/models/chats/GroupChat.ts
@@ -7,26 +7,9 @@ import {
   Table,
 } from "sequelize-typescript";
 import {getUUID} from "../../utils";
-import {Quest} from "../quest/Quest";
-import {QuestsResponse} from "../quest/QuestsResponse";
 import {Chat} from "./Chat";
 import {ChatMember} from "./ChatMember";
 
-@Scopes(() => ({
-  defaultScope: {
-    attributes: {
-      exclude: ["createdAt", "updatedAt"]
-    },
-    include: [{
-      model: Quest,
-      as: 'quest',
-      attributes: ["title"] // TODO Add Quest short
-    }]
-  },
-  idsOnly: {
-    attributes: ['employerId', 'workerId', 'questId', 'responseId', 'chatId']
-  }
-}))
 @Table
 export class GroupChat extends Model {
   @Column({primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID(), unique: true}) id: string;
