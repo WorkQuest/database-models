b70bfc954ec9a556d59a418cc806fa408e5c75be
Irina Prilepova <prilepovaid@mail.ru>
2022-02-16 14:53:59 +0700

    refactor(proposal): create proposalQuerySchema

--- src/schemes/proposal.ts
+++ src/schemes/proposal.ts
@@ -1,5 +1,5 @@
 import * as Joi from "joi";
-import {idSchema, transactionHashSchema} from "./common";
+import {idSchema, limitSchema, offsetSchema, transactionHashSchema} from "./common";
 import {ProposalStatus} from "../models";
 
 export const proposalNumberSchema = Joi.number().example(1).label('ProposalNumber');
@@ -10,7 +10,7 @@ export const proposerIdWalletSchema = Joi.string().example('0xe7489ba661e0487669
 export const proposerVotingPeriodSchema = Joi.string().example('1').label('ProposerVotingPeriod');
 export const proposalTimestampSchema = Joi.date().timestamp('unix').example(1631568392).label('ContractTimeStamp');
 export const nonceIdSchema = Joi.string().example("65464546452165556432245623").label('NonceId');
-
+export const proposalStatusesSchema = Joi.array().items(proposalStatus).label('ProposalStatuses');
 
 export const proposalSchema = Joi.object({
   id: idSchema,
@@ -40,3 +40,9 @@ export const allProposalsSchema = Joi.object({
   count: proposalNumberSchema,
   data: allProposalsDataSchema
 }).label('AllProposals');
+
+export const proposalQuerySchema = Joi.object({
+  status: proposalStatusesSchema,
+  limit: limitSchema,
+  offset: offsetSchema,
+}).label('ProposalQuery');
