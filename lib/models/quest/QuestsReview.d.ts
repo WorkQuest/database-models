7e2fe9a5ec609fb1a5984be37dfc35f4c852cd04
Microsxema <robertgtr@protonmail.com>
2022-02-21 11:48:49 +0700

    rename models

---
+++ lib/models/quest/QuestsReview.d.ts
@@ -0,0 +1,15 @@
+import { Model } from 'sequelize-typescript';
+import { User } from '../user/User';
+import { Quest } from './Quest';
+export declare class QuestsReview extends Model {
+    id: string;
+    questId: string;
+    fromUserId: string;
+    toUserId: string;
+    message: string;
+    mark: number;
+    fromUser: User;
+    toUser: User;
+    quest: Quest;
+    avgMark: number;
+}
