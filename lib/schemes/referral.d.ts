ca6f12d53683f3147ccf364660ad96b388f9a92c
Alexander Bokovoy <robzombieg@gmail.com>
2022-02-22 17:33:24 +0700

    update schemas (referrals)

--- lib/schemes/referral.d.ts
+++ lib/schemes/referral.d.ts
@@ -1,4 +1,5 @@
 import Joi = require("joi");
 export declare const referralWalletAddressSchema: Joi.StringSchema;
 export declare const referralAddressesSchema: Joi.ArraySchema;
+export declare const referralAffiliatesSchema: Joi.ArraySchema;
 export declare const referralAddAffiliatesSchemas: Joi.ObjectSchema<any>;
--- src/schemes/referral.ts
+++ src/schemes/referral.ts
@@ -5,6 +5,7 @@ import {mediaContentTypeSchema, mediaHashSchema} from "./media";
 
 export const referralWalletAddressSchema = Joi.string().label('WalletAddress').regex(/^0x[a-fA-F0-9]{40}$/).example('0x3e356dBeF7F3098407667a0f2aE6bC4ac9B69E0a');
 export const referralAddressesSchema = Joi.array().items(referralWalletAddressSchema).label('arrayReferralAddressSchema');
+export const referralAffiliatesSchema = Joi.array().items(idSchema).label('referralAffiliatesSchema');
 
 export const referralAddAffiliatesSchemas = Joi.object({
   v: accountAddressSchema,
