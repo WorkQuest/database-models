65d67b71b2ac7b32ca00478fb8423981b95445a3
Microsxema <robertgtr@protonmail.com>
2022-02-25 14:43:17 +0700

    add status QuestFactoryCreatedEvent

--- src/models/quest/QuestFactoryCreatedEvent.ts
+++ src/models/quest/QuestFactoryCreatedEvent.ts
@@ -1,5 +1,10 @@
-import {Column, DataType, Model, Table} from "sequelize-typescript";
 import {BlockchainNetworks} from "../types";
+import {Column, DataType, Model, Table} from "sequelize-typescript";
+
+export enum QuestFactoryStatus {
+  QuestEntityNotFound = -1,         /** The relationship of the quest entity with the quest on the contract -> Quest.nonce = QuestFactoryCreatedEvent.nonce */
+  Successfully = 0,                 /** Quest essence found by nonce (Quest.nonce = QuestFactoryCreatedEvent.nonce)                                         */
+}
 
 @Table /** Contract event */
 export class QuestFactoryCreatedEvent extends Model {
@@ -8,5 +13,7 @@ export class QuestFactoryCreatedEvent extends Model {
   @Column(DataType.STRING) employerAddress: string;
   @Column(DataType.STRING) contractAddress: string
   @Column(DataType.STRING) transactionHash: string;
-  @Column(DataType.STRING) network: BlockchainNetworks;
+
+  @Column({type: DataType.STRING, allowNull: false}) network: BlockchainNetworks;
+  @Column({type: DataType.INTEGER, allowNull: false}) status: QuestFactoryStatus;
 }
