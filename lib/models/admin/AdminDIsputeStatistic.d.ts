55666c36c5377b2226cbd1f118430ee1b2a88ce7
Microsxema <robertgtr@protonmail.com>
2022-02-28 13:05:11 +0700

    rebuild

--- lib/models/admin/AdminDIsputeStatistic.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from 'sequelize-typescript';
-import { Admin } from './Admin';
-export declare class AdminDIsputeStatistic extends Model {
-    id: string;
-    adminId: string;
-    resolvedDisputes: number;
-    averageResolutionTime: number;
-    admin: Admin;
-}
