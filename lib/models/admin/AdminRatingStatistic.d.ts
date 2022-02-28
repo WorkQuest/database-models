d54158cc84eb8605cd1316aa91b32f5adf3b3c9a
Microsxema <robertgtr@protonmail.com>
2022-02-28 13:25:19 +0700

    rebuild

--- lib/models/admin/AdminRatingStatistic.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from 'sequelize-typescript';
-import { Admin } from './Admin';
-export declare class AdminRatingStatistic extends Model {
-    id: string;
-    adminId: string;
-    reviewCount: number;
-    averageMark: number;
-    admin: Admin;
-}
