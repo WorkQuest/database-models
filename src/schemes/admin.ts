f58c039e22f872bf755d54c754ebc9167ef0508e
Microsxema <robertgtr@protonmail.com>
2022-02-28 14:01:08 +0700

    refractor statistics

--- src/schemes/admin.ts
+++ src/schemes/admin.ts
@@ -1,26 +1,21 @@
 import Joi = require("joi");
-import {idSchema} from "./common";
-import {AdminRole, HTTPVerb} from "../models";
-import {ratingStatisticAverageMarkSchema, ratingStatisticReviewCountSchema} from "./statistics";
+import {AdminActionMetadata, AdminRole} from "../models";
+import {HTTPVerbSchema, idSchema} from "./common";
 
 export const adminFirstNameSchema = Joi.string().max(255).example('Pavel').label('AdminFirstName');
 export const adminLastNameSchema = Joi.string().max(255).example('Durov').label('AdminLastName');
 export const adminEmailSchema = Joi.string().email().max(255).example('test@test.com').label('AdminEmail');
 export const adminPasswordSchema = Joi.string().min(8).max(255).label('AdminPassword'); // TODO: describe custom validator rule
 export const adminRoleSchema = Joi.string().valid(...Object.values(AdminRole)).default(AdminRole.main).example('main').label('AdminRole');
-export const isActiveSchema = Joi.boolean().example(true).label('AdminIsActive');
-export const adminActionMethodSchema = Joi.string().valid(...Object.values(HTTPVerb)).example(HTTPVerb.POST).label('AdminActionMethod');
+export const adminIsActiveSchema = Joi.boolean().example(true).label('AdminIsActive');
 export const adminActionPathSchema = Joi.string().example('/v1/admin/change/name').label('AdminActionRoute');
-export const resolvedQuestDisputesSchema = Joi.number().example(5).label('ResolvedDisputes');
-export const averageResolutionTimeInSecondsSchema = Joi.number().example(2693675.445).label('AverageResolutionTimeInSeconds');
-
 
 export const adminSchema = Joi.object({
   id: idSchema,
   email: adminEmailSchema,
   firstName: adminFirstNameSchema,
   lastName: adminLastNameSchema,
-  isActive: isActiveSchema,
+  isActive: adminIsActiveSchema,
   adminRole: adminRoleSchema,
 }).label('Admin');
 
@@ -29,20 +24,10 @@ export const adminWithSecretSchema = Joi.object({
   secret: Joi.string().max(255).example('HJRT4QCSGNHGSYLF'),
 }).label('RegisterAdminWithSecretSchema');
 
-export const adminActionSchema = Joi.object({
+export const adminActionMetadataSchema = Joi.object({
   id: idSchema,
   adminId: idSchema,
-  method: adminActionMethodSchema,
+  HTTPVerb: HTTPVerbSchema,
   path: adminActionPathSchema,
 }).label('AdminAction');
-
-export const adminQuestDisputesStatisticSchema = Joi.object({
-  id: idSchema,
-  adminId: idSchema,
-  reviewCount: ratingStatisticReviewCountSchema,
-  averageMark: ratingStatisticAverageMarkSchema,
-  resolvedQuestDisputes: resolvedQuestDisputesSchema,
-  averageResolutionTimeInSeconds: averageResolutionTimeInSecondsSchema,
-  admin: adminSchema,
-}).label('AdminQuestDisputesStatistic');
 
