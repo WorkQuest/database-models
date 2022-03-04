26e4a89d36efa260578848f2e8c4e7115426a877
Microsxema <robertgtr@protonmail.com>
2022-03-04 14:56:41 +0700

    rebuild

--- lib/models/quest/QuestBlockInfo.d.ts
+++
@@ -1,6 +0,0 @@
-import { Model } from "sequelize-typescript";
-import { BlockchainNetworks } from "../types";
-export declare class QuestBlockInfo extends Model {
-    lastParsedBlock: number;
-    network: BlockchainNetworks;
-}
