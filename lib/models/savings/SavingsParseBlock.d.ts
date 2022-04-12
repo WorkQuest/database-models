5e3010a5d55c355c7168070e3ff36b178d9ff7ed
hojttaler <adamoffilya@mail.ru>
2022-04-12 12:06:08 +0700

    Rebuild

--- lib/models/savings/SavingsParseBlock.d.ts
+++
@@ -1,6 +0,0 @@
-import { Model } from 'sequelize-typescript';
-import { BlockchainNetworks } from "../types";
-export declare class SavingsParseBlock extends Model {
-    lastParsedBlock: number;
-    network: BlockchainNetworks;
-}
