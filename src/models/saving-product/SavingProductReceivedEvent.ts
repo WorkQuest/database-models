c89f30a9268a1920abfe33a83b4b0d716be8f517
Microsxema <robertgtr@protonmail.com>
2022-04-14 15:55:14 +0700

    update timestamp

--- src/models/saving-product/SavingProductReceivedEvent.ts
+++ src/models/saving-product/SavingProductReceivedEvent.ts
@@ -7,7 +7,7 @@ export class SavingProductReceivedEvent extends Model {
   @Column(DataType.STRING) transactionHash: string;
   @Column(DataType.STRING) user: string;
   @Column(DataType.STRING) amount: string;
-  @Column(DataType.STRING) timestamp: string;
+  @Column(DataType.INTEGER) timestamp: string;
   @Column(DataType.STRING) event: string;
   @Column(DataType.STRING) network: BlockchainNetworks;
 }
