d91f14e0e7c2b911026315998379f110790c5448
Alexander Bokovoy <robzombieg@gmail.com>
2022-04-18 19:15:19 +0700

    [feature] add schemas liquidity wqt_weth

--- src/schemes/dailyLiquidity.ts
+++ src/schemes/dailyLiquidity.ts
@@ -3,11 +3,22 @@ import {blockNumberSchema, idSchema, timestampSchema} from "./common";
 
 export const poolSchema = Joi.string().example("281401").label("PoolSchema");
 
-export const dailyLiquiditySchema = Joi.object({
+export const dailyLiquidityWqtWbnbSchema = Joi.object({
   id: idSchema,
   date: timestampSchema,
   blockNumber: blockNumberSchema,
   bnbPool: poolSchema,
+  wqtPool: poolSchema,
+  usdPriceBNB: poolSchema,
+  usdPriceWQT: poolSchema,
+  reserveUSD: poolSchema,
+}).label('DailyLiquidity');
+
+export const dailyLiquidityWqtWethSchema = Joi.object({
+  id: idSchema,
+  date: timestampSchema,
+  blockNumber: blockNumberSchema,
+  ethPool: poolSchema,
   wqtPool: poolSchema,
   usdPriceBNB: poolSchema,
   usdPriceWQT: poolSchema,
