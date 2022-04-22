d9b675e7bd4ab7c615f48be0ae124d2c192f7ce5
Irina Prilepova <prilepovaid@mail.ru>
2022-04-22 16:09:29 +0700

    refactor(user): try another variant to work with array

--- lib/models/user/WorkerProfileVisibilitySetting.d.ts
+++ lib/models/user/WorkerProfileVisibilitySetting.d.ts
@@ -3,7 +3,7 @@ import { User } from './User';
 export declare class WorkerProfileVisibilitySetting extends Model {
     id: string;
     userId: string;
-    ratingStatusCanInviteMeOnQuest: number;
-    ratingStatusInMySearch: number;
+    ratingStatusCanInviteMeOnQuest: number[];
+    ratingStatusInMySearch: number[];
     user: User;
 }
