8d0580299e3a6ca2790d52943d4d68b5897e2b57
Ilya Kasimov <adamoffilya@mail.ru>
2022-04-25 19:42:18 +0700

    Add bridge schemes

---
+++ lib/schemes/bridge.d.ts
@@ -0,0 +1,3 @@
+import Joi from "joi";
+export declare const bridgeSymbolSchema: Joi.StringSchema;
+export declare const swapSchema: Joi.ObjectSchema<any>;
---
+++ src/schemes/bridge.ts
@@ -0,0 +1,19 @@
+import Joi from "joi";
+
+export const bridgeSymbolSchema = Joi.string().example('WQT').valid('WQT', 'USDT', 'BNB', 'ETH').example('BridgeSymbol');
+export const swapSchema = Joi.object({
+  canRedeemed: Joi.boolean().label('CanRedeemed'),
+  blockNumber: Joi.number().label('BlockNumber'),
+  transactionHash: Joi.string().label('TransactionHash'),
+  nonce: Joi.number().label('Nonce'),
+  timestamp: Joi.string().label('Timestamp'),
+  initiator: Joi.string().label('Initiator'),
+  recipient: Joi.string().label('Recipient'),
+  amount: Joi.string().label('Amount'),
+  chainTo: Joi.number().label('ChainTo'),
+  chainFrom: Joi.number().label('ChainFrom'),
+  symbol: bridgeSymbolSchema,
+  signData: Joi.array().items(Joi.string()).label('SignData'),
+}).label('Swap');
+
+const swapsSchema = Joi.array().items(swapSchema).label('Swaps');
