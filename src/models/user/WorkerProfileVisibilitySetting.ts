edb9d2eb44099248cd138c3a6215582e9e2b6fe3
Irina Prilepova <prilepovaid@mail.ru>
2022-04-25 11:23:18 +0700

    refactor(user): change EmployerProfileVisibility columns type to arrays

--- src/models/user/WorkerProfileVisibilitySetting.ts
+++ src/models/user/WorkerProfileVisibilitySetting.ts
@@ -15,9 +15,9 @@ export class WorkerProfileVisibilitySetting extends Model {
   @Column({ primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID() }) id: string;
 
   @ForeignKey(() => User)
-  @Column({type: DataType.STRING, allowNull: false}) userId: string;
+  @Column({ type: DataType.STRING, allowNull: false }) userId: string;
 
-  @Column({type: DataType.ARRAY(DataType.INTEGER), defaultValue: [RatingStatus.AllStatuses] }) ratingStatusCanInviteMeOnQuest: number[];
+  @Column({ type: DataType.ARRAY(DataType.INTEGER), defaultValue: [RatingStatus.AllStatuses] }) ratingStatusCanInviteMeOnQuest: number[];
   @Column({type: DataType.ARRAY(DataType.INTEGER), defaultValue: [RatingStatus.AllStatuses] }) ratingStatusInMySearch: number[];
 
   @BelongsTo(() => User) user: User;
