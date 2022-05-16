ca1f8b86238e461c76ac0ccc8afe75bce56a5d63
Alexander Bokovoy <robzombieg@gmail.com>
2022-05-16 17:55:18 +0700

    [feature] SwapUsdt fix models, add status

--- lib/models/SwapUsdt/SwapUsdtSendWqt.d.ts
+++ lib/models/SwapUsdt/SwapUsdtSendWqt.d.ts
@@ -1,12 +1,20 @@
 import { Model } from 'sequelize-typescript';
 import { BlockchainNetworks } from "../types";
-export declare class SwapUsdtSwapWqt extends Model {
+export declare enum swapUsdtStatus {
+    SwapCreated = "SwapCreated",
+    SwapActive = "SwapActive",
+    SwapProcessed = "SwapProcessed",
+    SwapCompleted = "SwapCompleted",
+    SwapError = "SwapError"
+}
+export declare class SwapUsdtSendWqt extends Model {
     txHash: string;
     txHashSwapInitialized: string;
     userId: string;
     blockNumber: number;
     ratio: number;
     network: BlockchainNetworks;
+    status: swapUsdtStatus;
     amount: string;
     gasUsed: string;
 }
--- src/models/SwapUsdt/SwapUsdtSendWqt.ts
+++ src/models/SwapUsdt/SwapUsdtSendWqt.ts
@@ -1,14 +1,23 @@
 import { Model, Column, DataType, Table } from 'sequelize-typescript';
 import { BlockchainNetworks } from "../types";
 
+export enum swapUsdtStatus {
+  SwapCreated = 'SwapCreated',
+  SwapActive = 'SwapActive',
+  SwapProcessed = 'SwapProcessed',
+  SwapCompleted = 'SwapCompleted',
+  SwapError = "SwapError"
+}
+
 @Table
-export class SwapUsdtSwapWqt extends Model {
+export class SwapUsdtSendWqt extends Model {
   @Column(DataType.STRING) txHash: string;
   @Column(DataType.STRING) txHashSwapInitialized: string;
   @Column(DataType.STRING) userId: string;
   @Column(DataType.INTEGER) blockNumber: number;
   @Column(DataType.INTEGER) ratio: number;
   @Column(DataType.STRING) network: BlockchainNetworks;
+  @Column(DataType.STRING) status: swapUsdtStatus;
   @Column(DataType.DECIMAL) amount: string;
   @Column(DataType.DECIMAL) gasUsed: string;
 }
