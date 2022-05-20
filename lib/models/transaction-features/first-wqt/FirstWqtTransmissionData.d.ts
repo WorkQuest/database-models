ac719ccdc0185af8a6442d62e1ed21a3ce185774
Microsxema <robertgtr@protonmail.com>
2022-05-20 14:17:09 +0700

    add status on FirstWqtTransmissionData

--- lib/models/transaction-features/first-wqt/FirstWqtTransmissionData.d.ts
+++ lib/models/transaction-features/first-wqt/FirstWqtTransmissionData.d.ts
@@ -1,6 +1,13 @@
 import { Model } from "sequelize-typescript";
+export declare enum TransmissionStatusFirstWqt {
+    BroadcastError = -2,
+    TransactionError = -1,
+    Pending = 0,
+    Success = 1
+}
 export declare class FirstWqtTransmissionData extends Model {
-    transactionHashTransferWqt: string;
+    transactionHashTransmissionWqt: string;
     txHashSwapInitialized: string;
+    status: TransmissionStatusFirstWqt;
     error: string;
 }
--- src/models/transaction-features/first-wqt/FirstWqtTransmissionData.ts
+++ src/models/transaction-features/first-wqt/FirstWqtTransmissionData.ts
@@ -1,12 +1,23 @@
 import {Column, DataType, ForeignKey, Model, Table} from "sequelize-typescript";
 import {SwapUsdtSwapTokenEvent} from "../../SwapUsdt/SwapUsdtSwapTokenEvent";
 
+
+export enum TransmissionStatusFirstWqt {
+  BroadcastError = -2,
+  TransactionError = -1,
+
+  Pending = 0,
+  Success = 1,
+}
+
 @Table
 export class FirstWqtTransmissionData extends Model {
-  @Column(DataType.STRING) transactionHashTransferWqt: string;
+  @Column(DataType.STRING) transactionHashTransmissionWqt: string;
 
   @ForeignKey(() => SwapUsdtSwapTokenEvent)
   @Column(DataType.STRING) txHashSwapInitialized: string;
+
+  @Column({ type: DataType.STRING, allowNull: false }) status: TransmissionStatusFirstWqt;
 
   @Column(DataType.STRING) error: string;
 }
