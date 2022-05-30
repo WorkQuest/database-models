7e252431036e8b1874d6b99f9b40aae471c5b129
Alexander Bokovoy <robzombieg@gmail.com>
2022-05-30 16:05:18 +0700

    [feature] add in supportPost status schema

--- src/schemes/support.ts
+++ src/schemes/support.ts
@@ -1,10 +1,13 @@
 import * as Joi from "joi";
 import { userEmailSchema } from "./user";
 import { idSchema } from "./common";
+import { AdminSupportResolved, SupportStatus } from "../models";
 
 export const titleSupportSchema = Joi.string().example('New support post').label('TitleSupport');
 export const descriptionSupportSchema = Joi.string().example('New support description in the post').label('DescriptionSupport');
 export const supportTicketSchema = Joi.number().example(725212).label('SupportTicket')
+export const supportPostStatusSchema = Joi.string().valid(...Object.values(SupportStatus)).example(SupportStatus).label("SupportPostStatus");
+export const supportPostDecisionSchema = Joi.string().valid(...Object.values(AdminSupportResolved)).example(AdminSupportResolved).label("SupportPostDecision");
 
 export const supportCreateSchema = Joi.object({
   email: userEmailSchema,
@@ -18,4 +21,6 @@ export const supportPostResponseSchema = Joi.object({
   email: userEmailSchema,
   title: titleSupportSchema,
   description: descriptionSupportSchema,
+  status: supportPostStatusSchema,
+  decision: supportPostDecisionSchema
 }).label('SupportPostResponse');
