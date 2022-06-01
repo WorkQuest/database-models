aa9e382bdfb41d4abacc3f82205bb91735066a55
Microsxema <robertgtr@protonmail.com>
2022-06-01 16:03:14 +0700

    rebuild

---
+++ lib/models/transaction-features/quest-dispute/QuestDisputeDecisionData.d.ts
@@ -0,0 +1,14 @@
+import { TransactionStatus } from "../types";
+import { Model } from "sequelize-typescript";
+export declare enum QuestDisputeDecision {
+    Rework = "Rework",
+    AcceptWork = "AcceptWork",
+    RejectWork = "RejectWork"
+}
+export declare class QuestDisputeDecisionData extends Model {
+    transactionHashDisputeResolution: string;
+    decision: QuestDisputeDecision;
+    gasPriceAtMoment: string;
+    status: TransactionStatus;
+    error: string;
+}
---
+++ lib/models/transaction-features/types.d.ts
@@ -0,0 +1,8 @@
+export declare enum TransactionStatus {
+    UnknownError = -3,
+    BroadcastError = -2,
+    TransactionError = -1,
+    Pending = 0,
+    InProcess = 1,
+    Success = 2
+}
