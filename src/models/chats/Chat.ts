53cc5928560c5a1ee0bd9d1a0b6165829b9c29f0
Microsxema <robertgtr@protonmail.com>
2022-06-02 18:00:51 +0700

    rebuild

--- src/models/chats/Chat.ts
+++ src/models/chats/Chat.ts
@@ -90,16 +90,6 @@ export class Chat extends Model {
   @Column({primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID(), unique: true}) id: string;
   @Column({type: DataType.STRING, allowNull: false}) type: ChatType;
 
-  // @ForeignKey(() => User) /** If group chat */
-  // @Column({type: DataType.STRING, defaultValue: null}) ownerUserId: string; //TODO/////////////////
-
-  // @ForeignKey(() => Message)
-  // @Column({type: DataType.STRING, defaultValue: null}) lastMessageId: string;
-  // @Column({type: DataType.DATE, defaultValue: null}) lastMessageDate: Date;
-
-  // @BelongsTo(() => Message, { foreignKey: 'lastMessageId', constraints: false }) lastMessage: Message;
-
-  // @HasMany(() => Message) messages: Message[];
   @HasMany(() => ChatMember) members: ChatMember[];
 
   @HasOne(() => ChatMember) meMember: ChatMember;
