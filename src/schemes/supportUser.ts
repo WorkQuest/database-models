55c6852d3adb4dcf5ddbb8701592641f43019e23
Alexander Bokovoy <robzombieg@gmail.com>
2022-06-07 13:04:22 +0700

    [feature] update build

--- src/schemes/supportUser.ts
+++ src/schemes/supportUser.ts
@@ -9,7 +9,6 @@ export const titleSupportTicketSchema = Joi.string().example('New support post')
 export const descriptionSupportTicketSchema = Joi.string().example('New support description in the post').label('DescriptionSupportTicket');
 export const statusSupportTicketSchema = Joi.number().valid(...Object.values(TicketStatus)).example(TicketStatus.Pending).label("StatusSupportTicket");
 export const postedDecisionSupportTicketSchema = Joi.string().valid(...Object.values(PostedDecision)).example(PostedDecision.OnEmailWithTheAuthor).label("PostedDecisionSupportTicket");
-
 export const supportTicketStatusesSchema = Joi.array().items(statusSupportTicketSchema).label('SupportTicketStatuses');
 
 export const supportTicketSchema = Joi.object({
