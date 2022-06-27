6b02a157ef9bacb448583d5a5720f0ada4edf0c4
Microsxema <robertgtr@protonmail.com>
2022-06-27 14:45:32 +0700

    rebuild

--- src/schemes/faucet.ts
+++
@@ -1,9 +0,0 @@
-import * as Joi from "joi";
-
-export const faucetStatusSchemas = Joi.string().example('true').label('FaucetStatus');
-export const transactionHashSchema = Joi.string().example('18vk40cc3er48fzs5ghqzxy88uq').label("TransactionHash");
-
-export const faucetSendWusdWqtResponseSchemas = Joi.object({
-  txHash: transactionHashSchema,
-  status: faucetStatusSchemas,
-}).label('FaucetSendWusdWqt');
