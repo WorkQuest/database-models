a9d912566a81243515eb8b3c53e142031ca76ccc
Microsxema <robertgtr@protonmail.com>
2022-09-14 12:00:19 +0700

    rebuild

--- lib/schemes/faucet.d.ts
+++
@@ -1,4 +0,0 @@
-import * as Joi from "joi";
-export declare const faucetStatusSchemas: Joi.StringSchema;
-export declare const transactionHashSchema: Joi.StringSchema;
-export declare const faucetSendWusdWqtResponseSchemas: Joi.ObjectSchema<any>;
